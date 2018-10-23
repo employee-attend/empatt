4d79d0ce6015ffd8001c64344ead63d6f571e01d
Shobuj <shobujsa93@gmail.com>
2018-10-23 12:38:50 +0600

    employee attendance system login and attend file created

---
+++ src/Admin/Login/Login.php
@@ -0,0 +1,78 @@
+<?php 
+namespace App\Admin\Login;
+
+if(!isset($_SESSION)){
+	session_start();
+}
+
+use App\Connection;
+use PDO;
+use PDOException;
+
+class Login extends Connection
+{
+	public function set($data = array()){
+		if(array_key_exists('username', $data)){
+			$this->username = $data['username'];
+		}
+		if(array_key_exists('password', $data)){
+			$this->password = $data['password'];
+		}
+		return $this;
+	}
+
+	// admin login 
+	public function adminLogin(){
+		try {
+
+			$password = $this->password;
+
+			$stmt = $this->con->prepare("select * from admin where username=:username ");
+			$stmt->bindValue(':username', $this->username, PDO::PARAM_STR);
+			//$stmt->bindValue(':password', $password, PDO::PARAM_STR);
+			$stmt->execute();
+			
+
+			if($stmt->rowCount() > 0){
+				$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+				if(password_verify($password, $row['password'])){
+					$_SESSION['username'] = $row['username'];
+					$_SESSION['password'] = $row['password'];
+					$_SESSION['admin']    = $row['id'];
+					$_SESSION['type']     = $row['administrattor'];
+
+					header('location: http://localhost/empatt/view/admin/index.php');
+				}
+
+				else{
+					$_SESSION['error'] = "password does not match !!!";
+					header('location: http://localhost/empattend/view/admin/login/index.php');
+				}
+				
+			}
+			else{
+				$_SESSION['error'] = "username  does not match !!!";
+				header('location: http://localhost/empattend/view/admin/login/index.php');
+			}
+			
+			/*if($stmt->rowCount() < 1){
+				$_SESSION['error'] = "Cannot find account with the username";
+			}
+			else{
+				$pass = md5($this->password);
+				$row = $stmt->fetchAll(PDO::PARAM_STR);
+				if($pass == $row['password']){
+					$_SESSION['admin'] = $row['id'];
+				}
+				else{
+					$_SESSION['error'] = "Incorrect Password";
+				}
+			}*/
+			
+		} catch (PDOException $e) {
+			echo "Error ".$e->getMessage()."<br>";
+			die();
+		}
+	}
+}
---
+++ view/front/includes/footer.php
@@ -0,0 +1,11 @@
+
+
+<!-- Bootstrap 3.3.7 -->
+<script src="assets/admin/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- Moment JS -->
+<script src="assets/admin/bower_components/moment/moment.js"></script>
+
+
+
+</body>
+</html>
