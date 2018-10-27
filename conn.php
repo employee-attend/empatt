72319d76a8f621346961a6a8ad7d55180281dc5f
shobuj islam <shobujislam1989@gmail.com>
2018-10-27 13:33:56 +0600

    database chenge in src/connection file

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'empatt');
+	$conn = new mysqli('localhost', 'root', '', 'employee_attend');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
